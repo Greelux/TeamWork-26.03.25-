05161abcc351c8c2de5ae88528c75738105f8da1
ikari <sydnazar08@gmail.com>
2025-06-23 20:34:03 +0300

    modelAppointment

--- TeamWork(26.03.25)/Models/Appointment.cs
+++ TeamWork(26.03.25)/Models/Appointment.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TeamWork_26._03._25_.Models
+namespace HealthMeet.Models
 {
-    internal class Appointment
+    public class Appointment
     {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public DateTime ScheduledAt { get; set; }
+        public string Status { get; set; }
     }
 }
