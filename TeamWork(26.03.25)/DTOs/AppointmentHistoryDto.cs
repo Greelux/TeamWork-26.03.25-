46196c3c6bc193cc18ecf3c73ee3d1123a28167b
ikari <sydnazar08@gmail.com>
2025-06-23 21:03:02 +0300

    AppointmentHistoryDto

--- TeamWork(26.03.25)/DTOs/AppointmentHistoryDto.cs
+++ TeamWork(26.03.25)/DTOs/AppointmentHistoryDto.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TeamWork_26._03._25_.DTOs
+namespace HealthMeet.DTOs
 {
-    internal class AppointmentHistoryDto
+    public class AppointmentHistoryDto
     {
+        public int AppointmentId { get; set; }
+        public string PatientUsername { get; set; }
+        public string DoctorUsername { get; set; }
+        public string SpecialtyName { get; set; }
+        public DateTime ScheduledAt { get; set; }
+        public string Status { get; set; }
     }
 }
